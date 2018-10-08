db2c9295de4896a784dfb28c050a08f7621a54e7
mister-corn <code@ronaldlibago.com>
2018-10-08 13:23:49 -0700

    21:23 GMT Knex-Redo

---
+++ src/db/migrations/20181008125556_create_expense_table.js
@@ -0,0 +1,38 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('expenses', (table) => {
+    table.increments('id');
+
+    table
+        .string('name', 128)
+        .notNullable();
+    
+    table
+        .timestamp('date')
+        .defaultTo(knex.fn.now());
+
+    table
+        .text('description');
+    
+    table
+        .float('amount')
+        .notNullable();
+    
+    table
+        .integer('budget_id')
+        .notNullable()
+        .references('id')
+        .inTable('budgets');
+    
+    table
+        .integer('cat_id')
+        .notNullable()
+        .references('id')
+        .inTable('categories');
+  });  
+};
+
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('expenses');
+};
---
+++ src/db/migrations/20181008125640_create_user_table.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id');
+    
+    table
+      .string('username', 128)
+      .notNullable();
+    
+    table
+        .timestamp('createdAt')
+        .defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};
